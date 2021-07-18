abe383b2ec64c0207ca03a50ffdac7330f06164d
Jaisidh Singh <75247817+jaisidhsingh@users.noreply.github.com>
2021-07-18 16:44:33 +0530

    Update index.js

--- TLDR/index.js
+++ TLDR/index.js
@@ -9,17 +9,11 @@ const PORT = process.env.PORT || 8000;
 const openaiURL = "https://api.openai.com/v1/engines/davinci/completions";
 
 
-// sublime text 4 testing mode seems to be working fine damn nigga this shit fast and tight
 const headers = {
   Authorization: 'Bearer '+ String(API_KEY),
   "Content-Type": "application/json",
 }
 
-const PROMPT =  "A neutron star is the collapsed core of a massive supergiant star, which had a total mass of between 10 and 25 solar masses, possibly more if the star was especially metal-rich.[1] Neutron stars are the smallest and densest stellar objects, excluding black holes and hypothetical white holes, quark stars, and strange stars.[2] Neutron stars have a radius on the order of 10 kilometres (6.2 mi) and a mass of about 1.4 solar masses.[3] They result from the supernova explosion of a massive star, combined with gravitational collapse, that compresses the core past white dwarf star density to that of atomic nuclei.\n\ntl;dr:"
-const body = {
-  "prompt": PROMPT,
-  "max_tokens": 60
-}
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -38,4 +32,3 @@ app.post('/api', (req, res)=>{
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-//this is a test in vim in windows terminal. working pretty well to be perfectly honest better than the older vim inside windows terminal
